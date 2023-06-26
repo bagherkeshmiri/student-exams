3564ba78391d0a778b5cc8203c9a1dc1608ef4a9
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-06-26 13:56:43 +0330

    optimize AdminStudent Controller

--- app/Http/Controllers/admin/AdminStudentController.php
+++ app/Http/Controllers/admin/AdminStudentController.php
@@ -6,109 +6,71 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\admin\student\AdminStudentEditRequest;
 use App\Http\Requests\admin\student\AdminStudentStoreRequest;
 use App\Models\User;
-use App\Repositories\User\UserRepositoryInterface;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
 class AdminStudentController extends Controller
 {
-    protected object $UserRepository;
-
-    public function __construct(UserRepositoryInterface $UserRepository)
-    {
-        $this->UserRepository = $UserRepository;
-    }
-
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $students = $this->UserRepository->paginate();
-        return view('admin.pages.student.list',compact('students'));
+        $students = User::paginate();
+        return view('admin.pages.student.list', compact('students'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        $levels = $this->UserRepository->getModel()->getStatuses();
-        return view('admin.pages.student.create',compact('levels'));
+        $levels = User::getStatuses();
+        return view('admin.pages.student.create', compact('levels'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(AdminStudentStoreRequest $request)
     {
-        $data = [
-            'name' => $request->input('name'),
-            'family' => $request->input('family'),
-            'username' => $request->input('username'),
-            'password' => $request->input('password'),
-            'level' => $request->input('level'),
-            'mobile' => $request->input('mobile'),
-        ];
         DB::beginTransaction();
         try {
-            $this->UserRepository->create($data);
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->family = $request->input('family');
+            $user->username = $request->input('username');
+            $user->password = $request->input('password');
+            $user->level = $request->input('level');
+            $user->mobile = $request->input('mobile');
+            $user->save();
             DB::commit();
-            return redirect()->back()->with('success','عملیات موفق');
-        } catch (Exception $error){
+            return redirect()->back()->with('success', __('errors.successful_operation'));
+        } catch (Exception) {
             DB::rollBack();
-            return redirect()->back()->with('error','خطا در عملیات ');
+            return redirect()->back()->with('error', __('errors.error_in_operation'));
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(User $user)
     {
-        $levels = $this->UserRepository->getModel()->getStatuses();
-        return view('admin.pages.student.edit',compact('user','levels'));
+        $levels = User::getStatuses();
+        return view('admin.pages.student.edit', compact('user', 'levels'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(AdminStudentEditRequest $request, User $user)
     {
-        $data = [
-            'name' => $request->input('name'),
-            'family' => $request->input('family'),
-            'username' => $request->input('username'),
-            'level' => $request->input('level'),
-            'mobile' => $request->input('mobile'),
-        ];
         DB::beginTransaction();
         try {
-            $this->UserRepository->update($data,$user->id);
+            $user->name = $request->input('name');
+            $user->family = $request->input('family');
+            $user->username = $request->input('username');
+            $user->level = $request->input('level');
+            $user->mobile = $request->input('mobile');
+            $user->save();
             DB::commit();
-            return redirect()->back()->with('success','عملیات موفق');
-        } catch (Exception $error){
+            return redirect()->back()->with('success', __('errors.successful_operation'));
+        } catch (Exception) {
             DB::rollBack();
-            return redirect()->back()->with('error','خطا در عملیات ');
+            return redirect()->back()->with('error', __('errors.error_in_operation'));
         }
-
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
-        $this->UserRepository->delete($user->id);
+        $user->delete();
         return redirect()->route('admin.user.index');
     }
 }
--- lang/fa/errors.php
+++ lang/fa/errors.php
@@ -4,4 +4,5 @@ return [
     'username_incorrect' => 'نام کاربری نادرست است',
     'password_incorrect' => 'رمز عبور نادرست است',
     'error_in_operation' => 'خطا در عملیات',
+    'successful_operation' => 'عملیات موفق',
 ];
