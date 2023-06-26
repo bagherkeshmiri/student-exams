db81bf42b4125d113b70d57e74a74cbaa80ac38d
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-06-26 14:13:45 +0330

    optimize AdminPermissionController

--- app/Http/Controllers/admin/AdminPermissionController.php
+++ app/Http/Controllers/admin/AdminPermissionController.php
@@ -4,90 +4,30 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Permission;
-use App\Repositories\Permission\PermissionRepositoryInterface;
-use App\Repositories\Role\RoleRepositoryInterface;
+use App\Models\Role;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class AdminPermissionController extends Controller
 {
-    protected object $PermissionRepository;
-    protected object $RoleRepository;
-
-    public function __construct(
-        PermissionRepositoryInterface $PermissionRepository,
-        RoleRepositoryInterface $RoleRepository,
-    )
-    {
-        $this->PermissionRepository = $PermissionRepository;
-        $this->RoleRepository = $RoleRepository;
-    }
-
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        $roles = $this->RoleRepository->all();
-        return view('admin.pages.permission.create',compact('roles'));
+        $roles = Role::all();
+        return view('admin.pages.permission.create', compact('roles'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $data = $request->input('permissions');
         DB::beginTransaction();
         try {
-            $this->PermissionRepository->createMany($data);
+            Permission::createMany($data);
             DB::commit();
-            return redirect()->back()->with('success','عملیات موفق');
-        } catch(Exception $error){
+            return redirect()->back()->with('success', __('errors.successful_operation'));
+        } catch (Exception) {
             DB::rollBack();
-            return redirect()->back()->with('error','خطا در عملیات ');
+            return redirect()->back()->with('error', __('errors.error_in_operation'));
         }
-
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Permission $permission)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Permission $permission)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Permission $permission)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Permission $permission)
-    {
-        //
     }
 }
