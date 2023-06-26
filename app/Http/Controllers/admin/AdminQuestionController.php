061ca157ee55e2116a9f7358c84d40c962d2af22
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-06-26 14:07:17 +0330

    optimize AdminQuestionController

--- app/Http/Controllers/admin/AdminQuestionController.php
+++ app/Http/Controllers/admin/AdminQuestionController.php
@@ -2,128 +2,95 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Enums\Questions\QuestionStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\admin\question\AdminQuestionEditRequest;
 use App\Http\Requests\admin\question\AdminQuestionStoreRequest;
+use App\Models\Admin;
 use App\Models\Question;
-use App\Repositories\Admin\AdminRepositoryInterface;
-use App\Repositories\Question\QuestionRepositoryInterface;
-use App\Repositories\User\UserRepositoryInterface;
+use App\Models\User;
 use Exception;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
+use LaravelIdea\Helper\App\Models\_IH_Admin_C;
 
 class AdminQuestionController extends Controller
 {
-    protected object $UserRepository;
-    protected object $AdminRepository;
-    protected object $QuestionRepository;
-
-    public function __construct(
-        UserRepositoryInterface $UserRepository,
-        AdminRepositoryInterface $AdminRepository,
-        QuestionRepositoryInterface $QuestionRepository
-    )
+    private function getAllAdmins(): array|Collection|_IH_Admin_C
     {
-        $this->UserRepository = $UserRepository;
-        $this->AdminRepository = $AdminRepository;
-        $this->QuestionRepository = $QuestionRepository;
+        return Admin::all();
     }
 
+    private function getAllUsers(): array|Collection|_IH_Admin_C
+    {
+        return User::all();
+    }
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $questions = $this->QuestionRepository->paginate();
-        $question_new_status = $this->QuestionRepository->getModel()::RAW;
-        return view('admin.pages.question.list',compact('questions','question_new_status'));
+        $questions = Question::paginate();
+        $question_new_status = QuestionStatus::Raw;
+        return view('admin.pages.question.list', compact('questions', 'question_new_status'));
     }
 
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        $admins = $this->AdminRepository->all()->pluck('id','full_name')->toArray();
-        $students = $this->UserRepository->all()->pluck('id','full_name')->toArray();
-        return view('admin.pages.question.create',compact('students','admins'));
+        $admins = $this->getAllAdmins()->pluck('id', 'full_name')->toArray();
+        $students = $this->getAllUsers()->pluck('id', 'full_name')->toArray();
+        return view('admin.pages.question.create', compact('students', 'admins'));
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(AdminQuestionStoreRequest $request)
     {
-        $data = [
-            'link' => $request->input('link'),
-            'response_deadline' => $request->input('response_deadline'),
-            'admin_id' => $request->input('admin_id'),
-            'text' => $request->input('text'),
-        ];
         DB::beginTransaction();
         try {
-            $question =  $this->QuestionRepository->create($data);
+            $question = new Question();
+            $question->link = $request->input('link');
+            $question->response_deadline = $request->input('response_deadline');
+            $question->admin_id = $request->input('admin_id');
+            $question->text = $request->input('text');
+            $question->save();
             $question->users()->attach($request->input('user_id'));
             DB::commit();
-            return redirect()->back()->with('success','عملیات موفق');
-        } catch (Exception $error){
+            return redirect()->back()->with('success', __('errors.successful_operation'));
+        } catch (Exception) {
             DB::rollBack();
-            return redirect()->back()->with('error','خطا در عملیات ');
+            return redirect()->back()->with('error', __('errors.error_in_operation'));
         }
     }
 
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Question $question)
     {
-        $admins = $this->AdminRepository->all()->pluck('id','full_name')->toArray();
-        $students = $this->UserRepository->all()->pluck('id','full_name')->toArray();
-        return view('admin.pages.question.edit',compact('question','admins','students'));
+        $admins = $this->getAllAdmins()->pluck('id', 'full_name')->toArray();
+        $students = $this->getAllUsers()->pluck('id', 'full_name')->toArray();
+        return view('admin.pages.question.edit', compact('question', 'admins', 'students'));
     }
 
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Question $question)
     {
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(AdminQuestionEditRequest $request, Question $question)
     {
-        $data = [
-            'link' => $request->input('link'),
-            'response_deadline' => $request->input('response_deadline'),
-            'admin_id' => $request->input('admin_id'),
-            'text' => $request->input('text'),
-        ];
         DB::beginTransaction();
         try {
-            $this->QuestionRepository->update($data,$question->id);
+            $question->link = $request->input('link');
+            $question->response_deadline = $request->input('response_deadline');
+            $question->admin_id = $request->input('admin_id');
+            $question->text = $request->input('text');
+            $question->save();
             $question->users()->sync($request->input('user_id'));
             DB::commit();
-            return redirect()->back()->with('success','عملیات موفق');
-        } catch (Exception $error){
+            return redirect()->back()->with('success', __('errors.successful_operation'));
+        } catch (Exception) {
             DB::rollBack();
-            return redirect()->back()->with('error','خطا در عملیات ');
+            return redirect()->back()->with('error', __('errors.error_in_operation'));
         }
     }
 
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Question $question)
     {
-        $this->QuestionRepository->delete($question->id);
+        $question->delete();
         return redirect()->route('admin.question.index');
     }
 }
