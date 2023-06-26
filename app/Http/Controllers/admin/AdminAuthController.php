56fe1836a3f90978b08cb9665be86d5178e76528
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-06-26 12:25:08 +0330

    optimize admin login

--- app/Http/Controllers/admin/AdminAuthController.php
+++ app/Http/Controllers/admin/AdminAuthController.php
@@ -4,55 +4,43 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\admin\auth\AdminLoginRequest;
-use App\Repositories\Admin\AdminRepositoryInterface;
-use Illuminate\Http\Request;
+use App\Models\Admin;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 
 class AdminAuthController extends Controller
 {
-    protected object $AdminRepository;
-
-    public function __construct(AdminRepositoryInterface $AdminRepository)
-    {
-        $this->AdminRepository = $AdminRepository;
-    }
-
-    public function showLogin()
+    public function showLogin(): Application|View|Factory
     {
         return view('admin.auth.login');
     }
 
-
     public function login(AdminLoginRequest $request)
     {
         try {
-            $admin = $this->AdminRepository->getModel()->where('username',$request->input('username'))->first();
-            if(is_null($admin) ) {
-                return redirect()->back()->with('error','نام کاربری نادرست است');
+            $admin = Admin::where('username', $request->input('username'))->first();
+            if (is_null($admin)) {
+                return redirect()->back()->with('error', __('errors.username_incorrect'));
             }
-            if(!Hash::check($request->input('password'),$admin->password)){
-                return redirect()->back()->with('error','رمز عبور نادرست است');
+            if (!Hash::check($request->input('password'), $admin->password)) {
+                return redirect()->back()->with('error', __('errors.password_incorrect'));
             }
             Auth::guard('admin')->loginUsingId($admin->id);
             return redirect()->route('admin.dashboard');
-        } catch (\Exception $error){
-            return redirect()->back()->with('error','خطا در عملیات');
+        } catch (Exception) {
+            return redirect()->back()->with('error', __('errors.error_in_operation'));
         }
-
     }
-
 
     public function logout()
     {
-        try {
-            Auth::guard('admin')->logout();
-            return redirect()->route('admin.login');
-        } catch (\Exception $error){
-            dd($error->getMessage());
-        }
-
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
     }
 
 }
