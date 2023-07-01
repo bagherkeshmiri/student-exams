de7d9ea711d7c12dbf616a2e4430d562beb24563
bagher keshmiri <bagherkeshmiri@gmail.com>
2023-07-01 20:19:10 +0330

    optimize manage admins

--- database/migrations/2022_07_19_105655_create_admins_table.php
+++ database/migrations/2022_07_19_105655_create_admins_table.php
@@ -4,33 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+return new class extends Migration {
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-
-            $table->string('name',50);
-            $table->string('family',100);
-            $table->string('username',30)->unique();
+            $table->string('name', 50);
+            $table->string('family', 100);
+            $table->string('username', 30)->unique();
             $table->string('password');
-
+            $table->string('password');
             $table->softDeletes();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('admins');
