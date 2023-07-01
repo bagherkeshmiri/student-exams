2830ef2fc64a60ecfc5c0bc1323134090a96534a
bagher keshmiri <bagherkeshmiri@gmail.com>
2023-07-01 20:45:26 +0330

    optimize seeders

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -3,39 +3,38 @@
 namespace Database\Seeders;
 
 use App\Models\Permission;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PermissionSeeder extends Seeder
 {
     public function run()
     {
-        Permission::create([
+        Permission::query()->create([
             'fa_name' => 'ویرایش کاربر',
             'en_name' => 'edit-user',
         ]);
 
-        Permission::create([
+        Permission::query()->create([
             'fa_name' => 'حذف کاربر',
             'en_name' => 'delete-user',
         ]);
 
-        Permission::create([
+        Permission::query()->create([
             'fa_name' => 'ایجاد کاربر',
             'en_name' => 'create-user',
         ]);
 
-        Permission::create([
+        Permission::query()->create([
             'fa_name' => 'ایجاد ادمین',
             'en_name' => 'create-admin',
         ]);
 
-        Permission::create([
+        Permission::query()->create([
             'fa_name' => 'حذف ادمین',
             'en_name' => 'delete-admin',
         ]);
 
-        Permission::create([
+        Permission::query()->create([
             'fa_name' => 'ویرایش ادمین',
             'en_name' => 'edit-admin',
         ]);
