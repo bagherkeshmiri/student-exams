41a49c398be36b691e02d8b25e032dfbc5921f16
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-07-05 09:54:52 +0330

    optimize and debug question correction

--- app/Http/Controllers/admin/AdminCorrectionController.php
+++ app/Http/Controllers/admin/AdminCorrectionController.php
@@ -7,6 +7,7 @@ use App\Enums\Questions\QuestionStatus;
 use App\Http\Requests\admin\correction\AdminCorrectionStoreRequest;
 use App\Models\Answer;
 use App\Models\Question;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
@@ -14,7 +15,6 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminCorrectionController extends Controller
 {
-
     public function index()
     {
         $questions = Auth::guard('admin')->user()->answeredQuestions();
@@ -23,30 +23,27 @@ class AdminCorrectionController extends Controller
 
     public function show(Question $question)
     {
-        $answer_status = getQuestionstatuses();
+        $answer_status = getAnswerStatuses();
         return view('admin.pages.correction.create', compact('question', 'answer_status'));
     }
 
     public function store(AdminCorrectionStoreRequest $request, Question $question)
     {
-        $data = [
-            'incorrect_statement' => $request->input('incorrect_text'),
-            'correct_statement' => $request->input('correct_text'),
-            'status' => $request->input('answer_status'),
-        ];
         DB::beginTransaction();
         try {
-            $question->answer->update($data);
-            if ($data['status'] == AnswerStatus::Corrected || $data['status'] == AnswerStatus::OkConfirm) {
-                $question->update([
-                    'status' => QuestionStatus::Confirmed,
-                    'confirmation_time' => now(),
-                ]);
+            $answer = new Answer();
+            $answer->incorrect_statement = $request->input('incorrect_text');
+            $answer->correct_statement = $request->input('correct_statement');
+            $answer->status = $request->input('answer_status');
+            $answer->text = $question->answer->text;
+            $question->answer()->save($answer);
+            if ($request->input('answer_status') == AnswerStatus::Corrected || $request->input('answer_status') == AnswerStatus::OkConfirm) {
+                $question->status = QuestionStatus::Confirmed;
+                $question->confirmation_time = Carbon::now();
             } else {
-                $question->update([
-                    'status' => QuestionStatus::Reviewd,
-                ]);
+                $question->status = QuestionStatus::Reviewd;
             }
+            $question->save();
             DB::commit();
             return redirect()->back()->with('success', __('errors.successful_operation'));
         } catch (Exception) {
