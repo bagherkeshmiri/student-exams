2c8c0cc6d8e9b6ab2c0f45835bba783b854cdd0e
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-07-05 10:13:16 +0330

    optimize

--- app/Http/Controllers/user/UserProtestController.php
+++ app/Http/Controllers/user/UserProtestController.php
@@ -4,27 +4,27 @@ namespace App\Http\Controllers\user;
 
 use App\Enums\Questions\QuestionStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Protest;
 use App\Models\Question;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserProtestController extends Controller
 {
-
     public function store(Request $request, Question $question)
     {
-        $data = [
-            'text' => $request->input('text'),
-            'question_id' => $question->id,
-        ];
         DB::beginTransaction();
         try {
-            $question->protest()->create($data);
-            $question->update([
-                'status' => QuestionStatus::HaveProtest,
-                'protest_time' => now()
-            ]);
+            $protest = new Protest();
+            $protest->text = $request->input('text');
+            $question->protest()->save($protest);
+
+            $question->status = QuestionStatus::HaveProtest;
+            $question->protest_time = Carbon::now();
+            $question->save();
+
             DB::commit();
             return redirect()->back()->with('success', __('errors.successful_operation'));
         } catch (Exception) {
